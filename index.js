3a38a8af78f518286d1b9a6635c5b6b3e4624554
moya <sy_moya@163.com>
2022-10-14 11:33:13 +0800

    Update index.js
    update context

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 window.onload = function() {
-  document.getElementById('main-content').innerHTML = '破琵琶天下第一 :)'
+  document.getElementById('main-content').innerHTML = '冯妇虎，叶公龙，舞蝶对鸣蛩[插图]。衔泥双紫燕，课蜜几黄蜂。'
 }
